9924268fbbe0384fdab65bf51c4f4bb732916846
Brenainn Vanhaverbeke <brenainnvanhaverbeke@gmail.com>
2022-08-20 17:03:57 +0200

    Completed 1st version of Enemy Manager. Handles active enemy characters and their spawners. Enemies despawn when too far away from player or enemy health is depleted. Spawners create enemies when player gets close enough, resets when player moves away far enough from them.

--- Super Castlevania 4/EnemyManager.cpp
+++ Super Castlevania 4/EnemyManager.cpp
@@ -1,8 +1,94 @@
 #include "pch.h"
 #include "EnemyManager.h"
 #include "Character.h"
+#include "EnemySpawner.h"
+#include "utils.h"
+#include <iostream>
+
+EnemyManager::EnemyManager()
+	: m_pEnemies{}
+	, m_pSpawners{}
+	, m_DespawnDistance{ 400.0f }
+{
+	m_pSpawners.push_back(new EnemySpawner("Bat", Point2f{ 200, 100 }, 0));
+	m_pSpawners.push_back(new EnemySpawner("Bat", Point2f{ 250, 100 }, 0));
+	m_pSpawners.push_back(new EnemySpawner("Bat", Point2f{ 300, 100 }, 0));
+}
 
 EnemyManager::~EnemyManager()
+{
+	DeleteEnemies();
+	DeleteSpawners();
+}
+
+void EnemyManager::Update(float elapsedSec, Character* player)
+{
+	UpdateEnemies(elapsedSec, player);
+	UpdateSpawners(elapsedSec, player);
+}
+
+void EnemyManager::Draw(int zIndex) const
+{
+	size_t nrOfEnemies{ m_pEnemies.size() };
+	for (size_t i{ 0 }; i < nrOfEnemies; ++i)
+		m_pEnemies.at(i)->Draw(zIndex);
+	size_t nrOfSpawners{ m_pSpawners.size() };
+	for (size_t i{ 0 }; i < nrOfSpawners; ++i)
+		m_pSpawners.at(i)->Draw(zIndex);
+}
+
+void EnemyManager::HandleAttack(const Rectf& weaponShape, int weaponDamage)
+{
+	size_t nrOfEnemies{ m_pEnemies.size() };
+	for (size_t i{ 0 }; i < nrOfEnemies; ++i)
+	{
+		Character*& enemy{ m_pEnemies.at(i) };
+		if (enemy->IsOverlapping(weaponShape))
+		{
+			enemy->TakeDamage(weaponDamage);
+		}
+	}
+}
+
+void EnemyManager::UpdateEnemies(float elapsedSec, Character* player)
+{
+	size_t nrOfEnemies{ m_pEnemies.size() };
+	for (size_t i{ 0 }; i < nrOfEnemies; ++i)
+	{
+		Character*& enemy{ m_pEnemies.at(i) };
+		m_pEnemies.at(i)->Update(elapsedSec);
+		if (ShouldEnemyDespawn(enemy, player))
+		{
+			std::cout << "Despawning enemy." << std::endl;
+			nrOfEnemies--;
+			std::swap(enemy, m_pEnemies.back());
+			delete m_pEnemies.back();
+			m_pEnemies.back() = nullptr;
+			m_pEnemies.pop_back();
+		}
+	}
+}
+
+void EnemyManager::UpdateSpawners(float elapsedSec, Character* player)
+{
+	size_t nrOfSpawners{ m_pSpawners.size() };
+	for (size_t i{ 0 }; i < nrOfSpawners; ++i)
+	{
+		EnemySpawner*& spawner{ m_pSpawners.at(i) };
+		spawner->Update(elapsedSec, player);
+		if (spawner->ShouldSpawn())
+			m_pEnemies.push_back(spawner->Spawn(player));
+	}
+}
+
+bool EnemyManager::ShouldEnemyDespawn(Character* enemy, Character* player)
+{
+	float distance{ utils::GetDistance(enemy->GetShape().GetCenter(), player->GetShape().GetCenter()) };
+	
+	return enemy->GetHealth() <= 0 || m_DespawnDistance < distance;
+}
+
+void EnemyManager::DeleteEnemies()
 {
 	size_t nrOfEnemies{ m_pEnemies.size() };
 	for (size_t i{ 0 }; i < nrOfEnemies; ++i)
@@ -13,16 +99,17 @@ EnemyManager::~EnemyManager()
 	m_pEnemies.clear();
 }
 
-void EnemyManager::Update(float elapsedSec)
+void EnemyManager::DeleteSpawners()
 {
-	size_t nrOfEnemies{ m_pEnemies.size() };
-	for (size_t i{ 0 }; i < nrOfEnemies; ++i)
-		m_pEnemies.at(i)->Update(elapsedSec);
+	size_t nrOfSpawners{ m_pSpawners.size() };
+	for (size_t i{ 0 }; i < nrOfSpawners; ++i)
+	{
+		delete m_pSpawners.at(i);
+		m_pSpawners.at(i) = nullptr;
+	}
+	m_pSpawners.clear();
 }
 
-void EnemyManager::Draw(int zIndex) const
+void EnemyManager::Update(float elapsedSec)
 {
-	size_t nrOfEnemies{ m_pEnemies.size() };
-	for (size_t i{ 0 }; i < nrOfEnemies; ++i)
-		m_pEnemies.at(i)->Draw(zIndex);
 }
--- Super Castlevania 4/EnemyManager.h
+++ Super Castlevania 4/EnemyManager.h
@@ -3,18 +3,33 @@
 #include "GameObject.h"
 
 class Character;
+class EnemySpawner;
 
 class EnemyManager final : public GameObject
 {
 public:
-	EnemyManager() = default;
+	EnemyManager();
 	~EnemyManager();
+
+	void Update(float elapsedSec, Character* player);
 
 	// Inherited via GameObject
 	virtual void Update(float elapsedSec) override;
 	virtual void Draw(int zIndex) const override;
 
-private:
-	std::vector<Character*> m_pEnemies;
+	void HandleAttack(const Rectf& weaponShape, int weaponDamage);
 
+private:
+	const float m_DespawnDistance;
+
+	std::vector<Character*> m_pEnemies;
+	std::vector<EnemySpawner*> m_pSpawners;
+
+	void UpdateEnemies(float elapsedSec, Character* player);
+	void UpdateSpawners(float elapsedSec, Character* player);
+
+	bool ShouldEnemyDespawn(Character* enemy, Character* player);
+
+	void DeleteEnemies();
+	void DeleteSpawners();
 };
