565485d114272cf766c1d5ee8619085f58ca729e
Brenainn Vanhaverbeke <brenainnvanhaverbeke@gmail.com>
2022-08-23 04:17:37 +0200

    Added pure IsOverlapping overload to add polygon overlap detection.

--- Super Castlevania 4/IOverlappingObject.h
+++ Super Castlevania 4/IOverlappingObject.h
@@ -4,4 +4,8 @@ class IOverlappingObject
 public:
 	virtual void CheckOverlap(const Rectf& overlappingShape) = 0;
 	virtual bool IsOverlapping(const Rectf& overlappingShape) = 0;
+	virtual bool IsOverlapping(const std::vector<Point2f>& overlappingShape) = 0;
+
+	// TODO: Consider possibility of extracting type of colliding object by passing an additional interface?
+	// virtual bool IsOverlapping(const MysteryObject& overlappingObject);
 };
